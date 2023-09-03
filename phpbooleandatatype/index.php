4dca01ef241ae8868ce6c7049da9f6ab2b1444c1
pro-mannan <promasudbd@gmail.com>
2023-09-03 16:21:15 +0600

    integer datatype

---
+++ phpbooleandatatype/index.php
@@ -0,0 +1,25 @@
+<?php 
+
+    // Boolean Data types
+    $isBoolean = 0;
+
+    // false value in php 
+    // int - 0, -0 = false
+    // floats = 0.0, -0.0 = false
+    // '0' = false
+    // [] = false           empty array is false value
+    // null = false         null value is false value
+
+
+    // define to condition here
+    if($isBoolean){
+        echo "Pass";
+    }else{
+        echo "Failed";
+    }
+
+
+
+
+
+?>
---
+++ phpntegerdatatype/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+    // integer Data types = 1-9, -0 -9;
+    
+    $bynary = decbin(33);
+    $hex = dechex(33);
+    $oct = decoct(33);
+    $hexdec = hexdec(33);
+
+    echo $bynary;
+    echo "<br>";
+    echo $hex;
+    echo "<br>";
+    echo $oct;
+    echo "<br>";
+    echo $hexdec;
+
+
+
+
+?>
