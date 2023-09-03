cd9b61b7620ce06d608bde451d9ce630122ef2d4
pro-mannan <promasudbd@gmail.com>
2023-09-03 19:08:36 +0600

    float datatype

---
+++ floatdatatype/index.php
@@ -0,0 +1,51 @@
+<?php 
+
+    // Floating number here
+    $num = 7.23;
+
+
+    // floor number 
+    // $fNum = 7.33;
+
+    // echo floor($fNum);
+
+
+
+
+
+    // cell number 
+    // $cNum = 7.33;
+
+    // echo ceil($cNum);
+
+
+
+
+
+    // condition floating to floating number 
+    // $x = 0.23;
+    // $y = 1 - 0.77;
+
+    // if($x === $y){
+    //     echo "yes";
+    // }else{
+    //     echo "false";
+    // }
+
+
+
+
+    // undefiend value is NAN = Not A Number
+    // echo NAN;
+
+    // -1 log then NAN 
+    echo log(1000);
+
+
+    // echo PHP_FLOAT_MAX * 2;
+
+
+
+
+
+?>
