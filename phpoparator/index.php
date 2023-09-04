5e3b404403630e03f716eb586dc589c0f7db0057
pro-mannan <promasudbd@gmail.com>
2023-09-04 11:07:56 +0600

    operator in basic php

---
+++ phpoparator/index.php
@@ -0,0 +1,29 @@
+<?php 
+    // OPERATORS
+    
+    // Arithmetic Operators (+ - * / **)
+
+    // Assignment Operators (= += -= /= %= **= )
+
+    // Staring Operators (. .=)
+
+    // Comparison Operators ( == === !== <> !=== < > <= >= <==> ?? ?:)
+
+    // Error Control Operators(@)
+
+    // Increment/ Decrement Operators (++ --)
+
+    // Logical Operators() (&& || ! and or xor)
+
+    // Bitwise Operators (& | ^ ~ << >>)
+
+    // Array Operators (+ == === !== <> !===)
+
+    // Execution Operators(``)
+
+    // Type Operators (instanceof)
+
+    // Nullsafe Operator - PHP8 (?)
+
+
+?>
