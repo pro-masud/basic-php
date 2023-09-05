165607581e6707af32ddb6cdc7944778bb0436f5
pro-mannan <promasudbd@gmail.com>
2023-09-05 08:16:20 +0600

    day 01 class number 1.4

--- hasinHyder/class01/index.php
+++ hasinHyder/class01/index.php
@@ -7,25 +7,25 @@
  * 
  */
 
-$name = 'bangladesh';
-$age = 16;
-$question = "How Are Your?";
+// $name = 'bangladesh';
+// $age = 16;
+// $question = "How Are Your?";
 
 
-$word = "age";
+// $word = "age";
 
 
-echo 'Hello'.$name. "" . $question;
+// echo 'Hello'.$name. "" . $question;
 
-echo "\n";
+// echo "\n";
 
-echo 'Hello $name';
+// echo 'Hello $name';
 
-echo "\n";
+// echo "\n";
 
-echo "Hello {$name}, {$question}\n";
+// echo "Hello {$name}, {$question}\n";
 
-echo "Hello {$name}, {$question}\n";
+// echo "Hello {$name}, {$question}\n";
 
 
 
@@ -37,5 +37,29 @@ echo "Hello {$name}, {$question}\n";
  * Day: 01    CLASS: 1.3
  * 
  */
+
+
+ $task = "Hello World";
+ echo $task;
+ echo "\n";
+
+ $task = "Pro World";
+ echo $task;
+ echo "\n";
+
+ #comment 
+ //comment
+ /*
+    Comments
+ */
+
+ /**
+  * multi pul line comment data here now
+ */
+
+define("PI", 3.14159);
+
+echo PI;
+
 
 
