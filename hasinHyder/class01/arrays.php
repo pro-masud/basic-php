5cebb746093f6f048c2406ab284981a5b428ff1b
pro-mannan <promasudbd@gmail.com>
2023-09-06 16:03:24 +0600

    array new function array_push(), array_pop(), array_shift()

--- hasinHyder/class01/arrays.php
+++ hasinHyder/class01/arrays.php
@@ -1,10 +1,35 @@
 <?php 
 
-    $num = '';
+    // $num = '';
 
 
     // student array here
-    $studnets = [
+    // $studnets = [
+    //     "masud",
+    //     "rana",
+    //     "mursalim",
+    //     "alok",
+    //     "papiya",
+    // ];
+
+    // $n = count($studnets);
+    // // for($i = 0; $i < $n; $i++ ){
+    // //     echo $studnets[$i] . "\n";
+    // // }
+
+    // for($i = $n - 1; $i >  1; $i--){
+    //     echo $studnets[$i] . "\n";
+    // }
+
+
+        
+    /**
+     * Now Working This Function => array_pop(), array_shift(), array_push, array_unshift() 
+     * 
+     * */ 
+
+
+     $studnets = [
         "masud",
         "rana",
         "mursalim",
@@ -12,10 +37,17 @@
         "papiya",
     ];
 
-    $n = count($studnets);
-    for($i = 0; $i < $n; $i++ ){
+    $newStudent = ["kamal"];
+
+    // array_push($studnets, "kamal", "jamal", "khali kothai akon");  array te data last a pathanor jno
+    // $studnets[] = "jamal";
+    // array_unshift($studnets, "promasud", "Dev Mausd", "goal Masud", "Jani Nah Masud Rana");  array data add to first here now
+   
+
+    for($i = 0; $i < count($studnets); $i++){
         echo $studnets[$i] . "\n";
     }
 
-
+    // $cutData = array_shift($studnets);
+    // $cutData = array_pop($studnets);
 ?>
