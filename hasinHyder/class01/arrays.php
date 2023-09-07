d9748f93b741eade2d850fcedf8f87fed58ce076
pro-mannan <promasudbd@gmail.com>
2023-09-07 15:51:41 +0600

    array complete today

--- hasinHyder/class01/arrays.php
+++ hasinHyder/class01/arrays.php
@@ -356,18 +356,236 @@
      * */ 
 
 
-      $arrayData =  ['masud rana', 'rana hosan', 'khalid hasan','ajim','php', 'javascript', 'go'];
+      // $arrayData =  ['masud rana', 'rana hosan', 'khalid hasan','ajim','php', 'javascript', 'go'];
 
 
     //   print_r($arrayData);
 
-      $arrayDataOne = array_slice($arrayData, 0, 3);
-      $arrayDataTwo = array_slice($arrayData, 3);
+    //   $arrayDataOne = array_slice($arrayData, 0, 3);
+    //   $arrayDataTwo = array_slice($arrayData, 3);
     
-    //  print_r($arrayDataOne);
-    //  print_r($arrayDataTwo);
+    // //  print_r($arrayDataOne);
+    // //  print_r($arrayDataTwo);
 
-    $arrayDataPlas = array_merge($arrayDataOne, $arrayDataTwo);
+    // $arrayDataPlas = array_merge($arrayDataOne, $arrayDataTwo);
 
-    print_r($arrayDataPlas);
+    // print_r($arrayDataPlas);
+
+
+
+
+
+
+
+
+    
+     /**
+     * array array_search(), in_array(), key_exists()
+     * */
+
+
+     $arrayData =  ['masud rana', 'rana hosan', 'khalid hasan','ajim','php', 'javascript', 'go'];
+
+     $number = [1, 4,3, 6,7, 45, 9, 33, 23, 99];
+     $keyNumbers = [
+        "a" => 1,
+        "b" =>  4,
+        "c" => 3,
+        "d" => 6,
+        "e" => 7,
+        "f" => 99];
+
+
+    //  if(in_array(9, $number, true)){
+    //     echo "number is exists now";
+    //  }else{
+    //     echo "Not Much This Array Value";
+    //  }
+
+
+    // if(array_search(99, $number, true)){
+    //   echo "This Number is a Right";
+    // }else{
+    //   echo "This Number Is Not Right";
+    // }
+
+    // $keyNumbers = [
+    //   "a" => 1,
+    //   "b" =>  4,
+    //   "c" => 3,
+    //   "d" => 6,
+    //   "e" => 7,
+    //   "f" => 99];
+
+    // if(key_exists("b", $keyNumbers)){
+    //     echo "This key Exists ??";
+    // }
+     
+
+
+
+
+
+
+/**
+ * array_intersect()   array এর ভ্যালু সাথে মিলবে সে গুলো রিটার্ন করবে
+ * array_intersect_assoc()
+ *  array_diff()  array এর ভ্যালু সাথে মিলবে না সে গুলো রিটার্ন করবে
+ *  array_diff_assoc()
+ * */
+
+     
+      
+    // $keyNumbers = [ "a" => 1, "b" =>  4, "c" => 3, "d" => 6, "e" => 7, "f" => 99 ];
+    // $numberOne = [ 1, 6, 5, 8, 3, 6, 0, 9, 3, 2, 9];
+    // $numberTwo = [ 1, 2, 3, 4, 2, 65, 234, 6, 8, 9, 7];
+
+
+    // // $MuchValue  = array_intersect($numberOne , $keyNumbers);
+    // // $MuchValue  = array_intersect_assoc($numberOne , $numberTwo);
+    // // print_r($MuchValue);
+
+
+
+    // // $noMuchValue = array_diff($numberOne, $numberTwo);
+    // $noMuchValue = array_diff_assoc($numberOne, $numberTwo);
+
+    // print_r($noMuchValue);
+
+
+
+
+
+
+
+
+
+/**
+* array_walk($array_var, “fun_name”)
+* array_map(“fun_name”, $array_var)
+* array_filter($array_var, “fun_name”)
+* */
+
+
+$numberOne = [ 1, 6, 5, 8, 3, 6, 0, 9, 3, 2, 9];
+$numberTwo = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+// function squaer($n){
+//   printf("this number is %s squer %s"."\n", $n, $n * $n);
+// }
+// $sQuaer = array_walk($numberTwo, 'squer');
+// print_r($sQuaer);
+
+function cube($n){
+  return $n * $n * $n;
+}
+
+// $newArray = array_map("cube", $numberTwo);
+
+function event($n){
+  return $n % 2 == 0;
+}
+
+function odd($n){
+  return $n % 2 == 1;
+}
+
+// $newArray = array_filter($numberTwo, "event");
+// $newArray = array_filter($numberTwo, "odd");
+
+// print_r($newArray);
+
+
+// more filter array action
+
+
+// $nameArray = ['masud', 'masruf', 'maruf', 'rana', 'mursalim', 'khirul', 'islam', 'pathor'];
+
+// function filtering($n){
+//   return $n[0] == 'p';
+// }
+
+// $newArray = array_filter($nameArray, 'filtering');
+
+// print_r($newArray);
+
+
+
+// $numberTwo = [ 1, 2, 3, 4, 5];
+
+// function someArray($old, $new){
+//   return $old + $new;
+// }
+
+// $newArrayValue = array_reduce($numberTwo, 'someArray');
+
+// echo $newArrayValue;
+
+
+
+
+
+
+
+/**
+ * list() function working here now
+*/
+
+// $mainArray = ['masud', 'rana', 'kasim', 'kamal'];
+
+// list($masud, $rana, $kasim, $kamal)  = $mainArray;
+
+// echo $masud;
+// echo "\n";
+// echo $rana;
+// echo "\n";
+// echo $kasim;
+// echo "\n";
+// echo $kamal;
+
+
+
+
+
+
+
+/**
+ * range() function working here now
+*/
+
+
+// $emptyArray = [];
+
+// for($i = 0; $i < 30; $i++){
+//   array_push($emptyArray, $i);
+// }
+
+
+// $arrayData = range(1, 20, 2);
+
+// foreach( range(0, 20, 2) as $singleNumber){
+//   echo $singleNumber."\n";
+// }
+
+// print_r($arrayData);
+
+
+
+
+
+
+
+
+
+/**
+ * mt_rand() function working 
+*/
+
+$rangeNumber = range(0, 999);
+
+$randomNumber = mt_rand(0, 999);
+echo $rangeNumber[$randomNumber];
+// print_r($rangeNumber);
+    
 ?>
