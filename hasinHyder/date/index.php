ad9e42a7195130cb7ba3e18ad3d549f76289017c
pro-mannan <promasudbd@gmail.com>
2023-10-01 17:11:48 +0600

    number factorial how much time to calculate here

--- hasinHyder/date/index.php
+++ hasinHyder/date/index.php
@@ -9,3 +9,23 @@
 // echo date('d/m/Y');  // output result is: 01/10/2023
 // echo date('d/F/Y');  // output result is: 01/October/2023
 // echo date('d/F/Y');  // output result is: 01/October/2023
+
+// startTime here 
+$startTime = microtime(true);
+factorial(100);
+sleep(2);
+$endTime = microtime(true);
+
+$fainalResult = $endTime - $startTime;
+
+printf("%10.8f", $fainalResult);
+
+
+//create a number factorial numbers
+function factorial($n){
+    $result = 1; 
+    for ($i = 1; $i<$n; $i++){
+        $result *= $i;
+    }
+    return $result;
+}
