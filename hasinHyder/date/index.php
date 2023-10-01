18c639a6d26be273f7a890ccf8febf1b12c29946
pro-mannan <promasudbd@gmail.com>
2023-10-01 17:03:50 +0600

    date and time somthing log functions

---
+++ hasinHyder/date/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+// time counting here now
+// echo time();    //1696155771
+// echo microtime(true); //1696155833.0876
+// echo microtime(false); //1696155833.0876
+// echo "\n";
+// echo date('d/m/y');  // output result is: 01/10/23
+// echo date('d/m/Y');  // output result is: 01/10/2023
+// echo date('d/F/Y');  // output result is: 01/October/2023
+// echo date('d/F/Y');  // output result is: 01/October/2023
