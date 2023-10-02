ac56871ede6c3f05a719e949e7e0ede776d205f2
pro-mannan <promasudbd@gmail.com>
2023-10-02 10:24:02 +0600

    public private working now

---
+++ hasinHyder/OOP/class.php
@@ -0,0 +1,36 @@
+<?php 
+
+//  ক্লাসের পাবলিক এবং প্রাইভেট মেথড
+
+class Fund{
+    public $fund;
+
+    function __construct($initialFund = 0){
+        $this ->fund = $initialFund;
+    }
+
+    public function addFund($money){
+        $this -> fund += $money;
+    }
+
+
+    public function deductFund($money){
+        $this -> fund -= $money;
+    }
+
+    public function totalFund(){
+        echo "Total fund is {$this -> fund}.";
+    }
+
+
+}
+
+$Ofund = new Fund(100);
+
+$Ofund -> addFund(10);
+echo "\n";
+$Ofund -> totalFund(100);
+echo "\n";
+$Ofund -> deductFund(9);
+echo "\n";
+$Ofund -> totalFund(100);
