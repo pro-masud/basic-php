83c832919ab70055f4e0e34f84a289d529d77593
pro-mannan <promasudbd@gmail.com>
2023-10-02 07:02:43 +0600

    update

--- hasinHyder/date/index.php
+++ hasinHyder/date/index.php
@@ -1,16 +1,16 @@
 <?php 
 
-// time counting here now
-// echo time();    //1696155771
-// echo microtime(true); //1696155833.0876
-// echo microtime(false); //1696155833.0876
-// echo "\n";
-// echo date('d/m/y');  // output result is: 01/10/23
-// echo date('d/m/Y');  // output result is: 01/10/2023
-// echo date('d/F/Y');  // output result is: 01/October/2023
-// echo date('d/F/Y');  // output result is: 01/October/2023
+//time counting here now
+echo time();    //1696155771
+echo microtime(true); //1696155833.0876
+echo microtime(false); //1696155833.0876
+echo "\n";
+echo date('d/m/y');  // output result is: 01/10/23
+echo date('d/m/Y');  // output result is: 01/10/2023
+echo date('d/F/Y');  // output result is: 01/October/2023
+echo date('d/F/Y');  // output result is: 01/October/2023
 
-// startTime here 
+//startTime here 
 $startTime = microtime(true);
 factorial(100);
 sleep(2);
@@ -29,3 +29,17 @@ function factorial($n){
     }
     return $result;
 }
+
+
+
+echo mktime(0,0,0,01,05,2001);
+echo "\n";
+echo strtotime("now");
+echo "\n";
+echo strtotime("+3days");
+echo "\n";
+echo strtotime("+2 weeks 7days 24 hours 86400 seconds") - strtotime("now");
+echo "\n";
+ 
+echo date('H:i:s ', strtotime('01 May 2001 12:12:08 PM'));
+  
