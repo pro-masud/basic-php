c2fb2ce12264a0d1de59a285f11b61b650e50bd6
pro-mannan <promasudbd@gmail.com>
2023-10-02 09:36:52 +0600

    __contruct function creating and working successfully o ji

--- hasinHyder/OOP/index.php
+++ hasinHyder/OOP/index.php
@@ -1,24 +1,83 @@
 <?php 
-class Human{
-   public $name;
-   public $age;
-   public $skill;
+// class Human{
+//    public $name;
+//    public $age;
+//    public $skill;
 
-   function humanInfo(){
-    echo "My name is {$this-> name} and {$this -> age} Years Old o Ji";
-   }
+//    function humanInfo(){
+//     echo "My name is {$this-> name} and {$this -> age} Years Old o Ji";
+//    }
+
+// //    function skills(){
+// //         echo "I'm a Full Stack WordPress Developer O ji";
+// //    }
+// }
+
+
+// // create a new object o ji 
+// $human = new Human;
+
+// $human -> name = "masud Rana";
+// $human -> age = "22";
+// $human -> skill = "Full Stack WordPress Developer";
+
+// echo $human -> humanInfo();
+
+
+// class Human{
+//    public $name;
+//    public $age;
+//    public $skill;
+
+//    function humanInfo(){
+//     echo "My name is {$this-> name} and {$this -> age} Years Old oh Ji and ";
+//     $this -> skills();
+//    }
 
 //    function skills(){
-//         echo "I'm a Full Stack WordPress Developer O ji";
+//         echo "I'm a Full Stack WordPress Developer Oh ji";
 //    }
-}
+// }
 
 
-// create a new object o ji 
-$human = new Human;
+// // create a new object o ji 
+// $human = new Human;
 
-$human -> name = "masud Rana";
-$human -> age = "22";
-$human -> skill = "Full Stack WordPress Developer";
+// $human -> name = "masud Rana";
+// $human -> age = "22";
+// $human -> skill = "Full Stack WordPress Developer";
 
-echo $human -> humanInfo();
+// echo $human -> humanInfo();
+
+
+class Human{
+    public $name;
+    public $age;
+    public $skill;
+
+    function __construct($name = "Masud Rana", $age = 12, $skill = "PHP Developer"){
+        $this -> name = $name;
+        $this -> age = $age;
+        $this -> skill = $skill;
+        
+        //humaninfo function invacate here
+        $this -> humanInfo();
+    }
+
+    function humanInfo(){
+        echo "My name is {$this-> name} and {$this -> age} Years Old oh Ji and ";
+        $this -> skills();
+    }
+
+    function skills(){
+        echo "I'm a Full Stack WordPress Developer Oh ji";
+    }
+ 
+
+ }
+ 
+ 
+ // create a new object o ji 
+ $human = new Human();
+
+ 
