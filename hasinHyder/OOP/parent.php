5d16a32ac82644cf1ce79cd4968d630cf76b331b
pro-mannan <promasudbd@gmail.com>
2023-10-05 10:30:56 +0600

    static parent method working to child method

---
+++ hasinHyder/OOP/parent.php
@@ -0,0 +1,19 @@
+<?php 
+
+class A{
+    static function sayHi(){
+        echo "Say Something One";
+    }
+}
+
+class B extends A{
+    static function sayHi(){
+        echo "Say Something two";
+        echo "\n";
+        parent::sayHi();
+    }
+}
+
+$newObj = new B();
+
+B::sayHi();
