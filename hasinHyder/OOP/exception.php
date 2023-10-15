408a928c0468e0ef29f3cee46d5be9eb63473a02
pro-mannan <promasudbd@gmail.com>
2023-10-15 08:48:38 +0600

    exception object

--- hasinHyder/OOP/exception.php
+++ hasinHyder/OOP/exception.php
@@ -1,18 +1,40 @@
 <?php 
-class Student{
-    public $name, $age;
-    function __construct($name, $age){
-        $this -> name = $name;
-        if($age<4){
-            throw new Exception("Invalid Age", 1001);
-        }
+// class Student{
+//     public $name, $age;
+//     function __construct($name, $age){
+//         $this -> name = $name;
+//         if($age<4){
+//             throw new Exception("Invalid Age", 1001);
+//         }else if($age > 30){
+//             throw new Exception("Too Youger", 1002);
+//         }
 
-        $this -> age = $age;
-    }
+//         $this -> age = $age;
+//     }
+// }
+
+// try{
+//     $s = new Student("Masud Rana", 32);
+// }catch(Exception $e){
+//     echo $e -> getCode() .":". $e -> getMessage();
+// }
+
+
+/**
+ * how to create a custom exception in php 
+ * */
+class MyException extends Exception{}
+class NetworkException extends Exception{}
+
+function testExceptions(){
+    throw new NetworkException();
 }
 
+
 try{
-    $s = new Student("Masud Rana", 2);
-}catch(Exception $e){
-    echo $e -> getCode() .":". $e -> getMessage();
+    testExceptions();
+}catch(MyException $e){
+    echo "My Exception";
+}catch(NetworkException $e){
+    echo "Network Exception";
 }
